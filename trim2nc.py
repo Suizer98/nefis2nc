3abc08c1c51d4379b5c8285464c54fc61a3324cd
Sui Zer <teysuizer1998@gmail.com>
2023-10-16 19:08:11 +0800

    fixed typo for adding attribute 'title'

--- trim2nc.py
+++ trim2nc.py
@@ -198,7 +198,7 @@ def create_organised_trimnc(datfile, deffile, outputfile):
     # latitude = rollrep(latitude)
 
     # Add global attribute
-    dataset.setncattr("title", "NetCDF created from NEFIS-file {}".format(datfile))
+    dst_ds.setncattr("title", "NetCDF created from NEFIS-file {}".format(datfile))
 
     # Add dimensions to the dataset
     dst_ds.createDimension("time", len(time))
